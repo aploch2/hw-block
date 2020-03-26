c7b38b65ea95b3b89c82332fc5b77d7e23a8e323
Austin Ploch <aploch2@my.wctc.edu>
2020-03-26 17:07:10 -0500

    initial plugin files created

---
+++ plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: HW Block
+ * Plugin URI: https://github.com/ahmadawais/create-guten-block/
+ * Description: A block for homework
+ * Author: Austin Ploch
+ * Author URI: 
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package hw
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block Initializer.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
